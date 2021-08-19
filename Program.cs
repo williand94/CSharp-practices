f3a0eee26df52ba398794f448e2681c627b3283a
Willian David <williand9415@gmail.com>
2021-08-18 21:05:16 -0500

    Primeros pasos en C#

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace primeraApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 22;
+
+            if (edad > 18)
+            {
+                Console.WriteLine("Puedes salir a Follar tienes " + edad);
+            }else
+            {
+                Console.WriteLine("Se queda en casa mi rey");
+            }
+
+        }
+    }
+
+}
+/* 
+namespace secondApp{
+    
+    class Program{
+        static void Main(string[] args){
+
+        }
+    }
+
+} */
