ce4d6fb47b00cd219c2433061698ac576b026803
Willian David <williand9415@gmail.com>
2021-09-04 00:53:59 -0500

    Estructuras de control CSharp

--- Program.cs
+++ Program.cs
@@ -6,17 +6,80 @@ namespace primeraApp
     {
         static void Main(string[] args)
         {
-            int edad = 22;
-
+            /* int edad = 22;
             if (edad > 18)
             {
                 Console.WriteLine("Puedes salir a Follar tienes " + edad);
             }else
             {
                 Console.WriteLine("Se queda en casa mi rey");
+            } */
+/* 
+            var age = 22;
+            age = 221;
+            Console.WriteLine($"Tienes una edad de {age} ");
+            
+            int a;
+            int b;
+            int c;
+
+            a = b = c = 17;
+            Console.WriteLine($"Printing {c}"); */
+
+            //Conversión explícita
+           /*  double temperatura = 33.9;
+            Console.WriteLine(temperatura);
+            int myTemperatura = (int) temperatura;
+            Console.WriteLine(myTemperatura); */
+            //Conversión implícita
+            //Este tipoi de conversiones no necesita casting, ya que son tipos compatibles
+            /* int dinero = 10000000;    
+
+            long myMoney = dinero;
+
+            float cm = 1.96F;
+
+            double altura = cm; */
+
+            /* Console.WriteLine("Ingrese primer número para la operación:");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese segundo número para la operación:");
+            int number2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"La suma es: {number1 + number2}"); */
+/* 
+            const int VALOR = 5;
+            const int VALOR1 = 4;
+
+            System.Console.WriteLine("Valor: {0}-{1}",VALOR,VALOR1); 
+        */
+
+/*             const double PI = 3.1416;
+
+            System.Console.WriteLine("Ingrese el radio");
+            
+            double radio = double.Parse(Console.ReadLine());
+
+            //double area = radio * radio * PI;
+
+            double area = Math.Pow(radio,2) * PI;
+
+            System.Console.WriteLine($"El área es : {area}"); */
+    
+            double calcularArea(double a, double b){
+
+                double result =  a + b;
+                
+
+
+                return result ;
+
             }
 
+            //Console.ReadLine(calcularArea(3.5,6.0));
+            System.Console.WriteLine(calcularArea(3.5,6.0));
+
         }
+
     }
 
 }
---
+++ metodos/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace metodos
+{
+    class metodos
+    {
+        static void Main(string[] args)
+        {
+
+            Random numero = new Random();
+
+            int nAleatorio = numero.Next(0,100);
+            int intentos = 0;
+            System.Console.WriteLine("Ingresa un número por favor!");
+            int nIngresado = Int32.Parse(Console.ReadLine());
+
+            while(nAleatorio != nIngresado)
+            {
+                if(nAleatorio < nIngresado)
+                {
+                    System.Console.WriteLine("Ingresa un número mas bajo");
+                    nIngresado = Int32.Parse(Console.ReadLine());
+                } 
+                else if (nAleatorio > nIngresado) 
+                {
+                    System.Console.WriteLine("Ingresa un número mas alto");
+                    nIngresado = Int32.Parse(Console.ReadLine());
+
+                }
+                intentos ++;
+            }    
+            System.Console.WriteLine($"EL juego terminó con {intentos} intentos. el número era {nAleatorio}");
+
+
+
+           /*  System.Console.WriteLine("Ingresa el mes a calcular");
+
+            int nMes = Int32.Parse(Console.ReadLine());
+
+            switch (nMes)
+            {
+                case 6:
+                    double primas = 0.02;
+                    double salario = 900000.90;
+                   Console.WriteLine(primas * salario);
+
+                    break;
+                default:
+                break;
+            } */
+
+            /* int valor1 = 5;
+            double valor2 = 6.9;
+            double valor3 = 8.9;
+            Console.WriteLine(suma(valor2,valor1, valor3));  */ 
+
+          /*   string carnet = "no";
+
+            System.Console.WriteLine("Iingresa tu edad");
+            int edad = Int32.Parse(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                System.Console.WriteLine("¿Tienes carnet?");
+                carnet = System.Console.ReadLine();
+                
+            }
+
+
+            if(edad >= 18 && carnet == "si")
+
+                System.Console.WriteLine("You can Drive");
+            else
+                System.Console.WriteLine("You don't drive!");
+
+ */
+        }
+        //static double suma(double a, int b, double c= 4) => a + b + c ;
+    
+        
+    
+    
+    
+    }
+
+}
